8f77dfe6516b6b7de616d890327eb5ca209fe726
hajjishirwac <shirwachajji@gmail.com>
2022-08-31 14:24:11 +0300

    Slice and Splice

--- index.js
+++ index.js
@@ -47,4 +47,15 @@ function titleCase(str) {
   }
   
   titleCase("I'm a little tea pot");
+
+//   Slice and Splice
+function frankenSplice(arr1, arr2, n) {
+    let localArray = arr2.slice();
+    for (let i = 0; i < arr1.length; i++) {
+      localArray.splice(n, 0, arr1[i]);
+      n++;
+    }
+    return localArray;
+  }
   
+  frankenSplice([1, 2, 3], [4, 5, 6], 1);
